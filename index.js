8ae94f734270163b6bedde83f5f95d50a8f13ccd
Ho <rbish005@BKUSL-1FP6P3XY.local>
2020-08-17 14:32:37 -0400

    added front end

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 
+app.use(express.static('public'));
 
 app.get('/products', async (_request, response) => {
   const products = await stripe.products.list({
@@ -14,7 +15,7 @@ app.get('/products', async (_request, response) => {
   const prices = await stripe.prices.list({
     limit: 100,
   });
-  
+
   prices.data.forEach(price => {
     const theAssociatedProduct = products.data.find(
       product => product.id === price.product
@@ -28,10 +29,11 @@ app.get('/products', async (_request, response) => {
     category: product.metadata.category,
     currency: product.price.currency,
     price_cents: product.price.unit_amount,
+    price_id: product.price.id,
   }));
   response.json(cleanedUpProducts);
 });
 
 
 
-app.listen(3000, () => console.log(`Server is on!`));
+app.listen(3000, () => console.log(`Server is up and running 🚀`));
